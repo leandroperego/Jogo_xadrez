3f8de28a019007f671b3d1870a791480d3ab5626
Leandro Perego <leperego@hotmail.com>
2022-06-22 19:02:52 -0300

    Primeira classe criada. Classe Posicao

---
+++ src/tabuleiro/Posicao.java
@@ -0,0 +1,27 @@
+package tabuleiro;
+
+public class Posicao {
+	
+	private int linha;
+	private int coluna;
+	
+	public Posicao(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+	
+	@Override
+	public String toString() {
+		return linha + ", " + coluna;
+	}
+	
+	
+}
